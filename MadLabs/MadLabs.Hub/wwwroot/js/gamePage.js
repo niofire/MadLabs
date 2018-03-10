a0707805672e94d186268cf080d8ff1847073387
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2018-03-10 12:46:30 -0800

    Add Screen locking on game page

---
+++ MadLabs/MadLabs.Hub/wwwroot/js/gamePage.js
@@ -0,0 +1,13 @@
+﻿console.log("Hello, World!");
+window.addEventListener("keydown", function (e) {
+
+    // Disable Space + Arrow keys
+    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+
+}, false);
+
+window.onload = function () {
+    document.getElementById("game-iframe").scrollIntoView(true);
+};
